e5501e5cb1b5cb79d3630fe23834e7548a757d02
summerstarchica <trudyanncampbell.402@gmail.com>
2016-11-02 14:14:58 +0000

    picture added to all tiles

--- fifteen.js
+++ fifteen.js
@@ -7,13 +7,14 @@ var puzzlepieces = document.querySelectorAll ("#puzzlearea div");
 
 var xpos=0;
 var ypos=0;
+//document.getElementById("overall").style.puzzlepieces.backgroundPosition = xpos * 100 + "px" + ypos -100 +"px";     
 
 for (var i=0; i < puzzlepieces.length; i++) {
     
     puzzlepieces[i].setAttribute("class","puzzlepiece");
-    puzzlepieces[i].style.left= xpos + 'px';
-    puzzlepieces[i].style.top= ypos + 'px';
-    puzzlepieces[i].style.backgroundPosition= "_" +xpos + "px" + ypos + "px";
+    puzzlepieces[i].style.left= xpos + 'px'; //get position of left puzzle piece
+    puzzlepieces[i].style.top= ypos + 'px'; // get position of top puzzle piece
+    puzzlepieces[i].style.backgroundPosition= xpos + "px " + ypos + "px";
     if (xpos <300) {
         xpos+=100;
     }
@@ -23,6 +24,11 @@ for (var i=0; i < puzzlepieces.length; i++) {
         ypos+=100;
     }
     }   
-            
-}
+    
+};
+
+
+
+
+
    
